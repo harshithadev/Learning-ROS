23921f4d7728fdfc64056edef5833ccc45ac3109
harshithadev <harshithadevineni16@gmail.com>
2025-01-23 10:00:11 +0530

    remove redundant package

--- bumperbot/build/bumperbot_package_py/prefix_override/sitecustomize.py
+++ bumperbot/build/bumperbot_package_py/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/harshdev/Documents/harsh/ROS/bumperbot/install/bumperbot_package_py'
+    sys.prefix = sys.exec_prefix = '/home/harshdev/Documents/harsh/Learning-ROS/bumperbot/install/bumperbot_package_py'
