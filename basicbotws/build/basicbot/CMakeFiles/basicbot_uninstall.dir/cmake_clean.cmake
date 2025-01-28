09bc39750c55b23f4b7b4a33902430dd75d2c612
harshithadev <harshithadevineni16@gmail.com>
2025-01-28 12:06:10 +0530

    base urdf of basic bot

---
+++ basicbotws/build/basicbot/CMakeFiles/basicbot_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/basicbot_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/basicbot_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
