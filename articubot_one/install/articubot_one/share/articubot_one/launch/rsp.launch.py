2eb097daabe2744c3a1310b8e797c1f8c730c77a
harshithadev <harshithadevineni16@gmail.com>
2025-02-12 16:01:00 +0530

    random changes, everything still works

--- articubot_one/install/articubot_one/share/articubot_one/launch/rsp.launch.py
+++ articubot_one/install/articubot_one/share/articubot_one/launch/rsp.launch.py
@@ -1,7 +1,5 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration, Command
 from launch.actions import DeclareLaunchArgument
