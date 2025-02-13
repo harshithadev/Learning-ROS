03b6b2760ef54abd742dd505effee6c759b56d70
harshithadev <harshithadevineni16@gmail.com>
2025-02-13 12:35:37 +0530

    remove redundant files

--- basicbotws/build/joy_to_cmdvel/CMakeFiles/joy_to_cmdvel_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/joy_to_cmdvel_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/joy_to_cmdvel_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
