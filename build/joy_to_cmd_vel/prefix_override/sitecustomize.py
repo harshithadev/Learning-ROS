b16282084627f9d246dcfe26c6d45e5d19e871e0
harshithadev <harshithadevineni16@gmail.com>
2025-03-10 10:18:13 +0530

    deleted log build and install folder

--- build/joy_to_cmd_vel/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/harshdev/Learning-ROS/install/joy_to_cmd_vel'
--- build/point_to_point_nav/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/harshdev/Learning-ROS/install/point_to_point_nav'
